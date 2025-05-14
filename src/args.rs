658be77afb65078a0ae04a4a882ae939aa704317
Carpetess <andre.filipe19062003@gmail.com>
2025-05-14 08:14:53 +0100

    Shell for the main code

--- src/args.rs
+++ src/args.rs
@@ -10,7 +10,7 @@ use clap:: {
 pub struct GuiltArgs{
     
     #[clap(subcommand)]
-    main_command: Command
+    pub main_command: Command
 
 }
 
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,34 @@
 mod args;
 
-use args::GuiltArgs;
+use args::{GuiltArgs, Command, CatFileCommand, HashObjectCommand, UpdateIndexCommand};
 use clap::Parser;
 fn main() {
     let args: GuiltArgs  = GuiltArgs::parse();
-    println!("{:?}", args);
+    match args.main_command {
+        Command::Init => {init();}
+        Command::WriteTree => {write_tree();}
+        Command::CatFile(cat_file_args)=> {cat_file(cat_file_args);}
+        Command::HashObject(hash_object_args) => { hash_object(hash_object_args);}
+        Command::UpdateIndex(update_index_args) => { update_index(update_index_args);}
+    }
+}
+
+fn init() {
+
+}
+
+fn cat_file(cat_file_args: CatFileCommand) {
+
+}
+
+fn hash_object(hash_object_args: HashObjectCommand) {
+
+}
+
+fn update_index(update_index_args: UpdateIndexCommand) {
+
+}
+
+fn write_tree() {
+
 }
