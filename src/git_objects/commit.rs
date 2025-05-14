f9fb20d91f088bda666009f0b609a116f8526c22
Carpetess <andre.filipe19062003@gmail.com>
2025-05-14 11:05:12 +0100

    Commit impl for git object done

--- src/git_objects/commit.rs
+++ src/git_objects/commit.rs
@@ -1,7 +1,24 @@
+use super::git_object::GitObject;
+
 pub struct Commit {
-    hashed_content: Vec<u8>
+    content: Vec<u8>,
+}
+
+impl Commit {
+    pub fn new() -> Self {
+        todo!()
+    }
 }
 
 impl GitObject for Commit {
-
+    fn pretty_print(&self) {
+        println! {"{}",  String::from_utf8(self.content.clone()).unwrap()}
+    }
+    fn format_object(&self) -> String {
+        format!(
+            "commit {}\0{}",
+            self.content.len(),
+            String::from_utf8(self.content.clone()).unwrap()
+        )
+    }
 }
--- src/git_objects/mod.rs
+++ src/git_objects/mod.rs
@@ -1,3 +1,4 @@
 pub mod git_object;
 pub mod blob;
 pub mod tree;
+pub mod commit; 
